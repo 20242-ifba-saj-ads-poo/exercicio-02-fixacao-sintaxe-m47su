4fc4571366748388fb3f9a740c1a755231224f42
matsu <102255791+m47su@users.noreply.github.com>
2024-11-13 19:46:01 -0300

    Add files via upload
    Exercicios de fixação

---
+++ Exercicio3.java
@@ -0,0 +1,17 @@
+
+//Imprima todos os múltiplos de 3, entre 1 e 100
+public class Exercicio3
+{
+	public static void main(String[] args) {
+
+		for (int numeros = 1; numeros <= 100; numeros++) {
+		    
+		    if(numeros % 3 == 0){
+		        System.out.println(numeros);
+		    }
+	}
+
+}
+}
+
+
